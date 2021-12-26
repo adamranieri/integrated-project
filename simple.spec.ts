ef94fc71c7f88f0fc9cb00157cb20a6c57536cb7
Adam Ranieri <adam.ranieri@revature.com>
2021-12-26 17:30:41 -0500

    added intial files

---
+++ simple.spec.ts
@@ -0,0 +1,18 @@
+import { add } from "."
+
+describe("Simple Suite", ()=>{
+
+    it("Sould sum 2 + 2", ()=>{
+        const sum = add(10,5);
+        expect(sum).toBe(15);
+    })
+
+    it("Should fail", ()=>{
+        expect(10).toBe(5);
+    })
+
+    it("should read an environment variable", ()=>{
+        expect(process.env.SOMETING).toBeTruthy()
+    })
+
+})
